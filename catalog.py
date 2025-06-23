3001e33bd50ed60f076f7dcecd135d733fa7eb2e
Jose Luis Gordillo Ruiz <jlgr@c3.unam.mx>
2025-06-23 11:02:09 -0600

    diferenciación entre ensambles 'espaciales' y 'de individuos'
    decidimos que los nombres adecuados para los tipos de ensambles son
    'espaciales' y de 'individuos'. ya veremos que cosas habrá que tomar
    en cuenta si de pronto un ensamble de 'individuos' se convierte en
    una base para fusionar diferentes fuentes de datos.

--- catalog.py
+++ catalog.py
@@ -3,24 +3,58 @@ from flask import Response
 import requests
 import pandas as pd
 import json
+import itertools
+
+url_spatial_ensembles = 'http://chilamdev.c3.unam.mx:5001/regions/region-grids/'
+
+def get_spatial_ensembles_names(url):
+    '''
+    obtenemos una lista de los ensambles registrados en el servicio de ensambles espaciales (regions/grids).
+    esto debería considerar el part footprint_region:resolution
+    a manera de ejemplo, por ahora solo usamos 'resolution'.
+    '''    
+    ensembles_dict = requests.get(url).json()
+    ensembles_DF = pd.json_normalize(ensembles_dict, record_path='data')
+    return ensembles_DF["resolution"].unique() # como hay varias regiones, las 'resolution' están repetidas.
 
 
-
-
-def completa(url):
+def get_spatial_ensembles_from_source(url, spatial_ensembles):
+    '''
+    a partir de la fuente (url) obtenemos la lista de 'ensambles espaciales' en las que la fuente proporciona 
+    variables.
+    '''
     if url:
         variables = pd.read_json(url  + '/variables')
-        datos  = { res:variables["available_grids"].apply(lambda x: res in x).sum() for res in resolutions}
-        print(datos)
-        return datos
+        ensembles = {ensemble:variables["available_grids"].apply(lambda x: ensemble in x).sum() for ensemble in spatial_ensembles}
+        return ensembles
     return {}
-        
-regions_dict = requests.get('http://chilamdev.c3.unam.mx:5001/regions/region-grids/').json()
-regions_DF = pd.json_normalize(regions_dict, record_path='data')
-resolutions = regions_DF["resolution"].unique()
 
-catalogo = pd.read_csv("catalogo.csv")
-catalogo.fillna("", inplace=True)
+def get_individuals_ensembles_from_source(url, spatial_ensembles):
+    '''
+    a partir de la fuente (url) obtenemos la lista de 'ensambles de individuos'
+    en los que la fuente proporciona variables. (cualquier ensamble que no está en la lista 'oficial' de ensambles  espaciales)
+    '''
+    if url:
+        variables = pd.read_json(url  + '/variables')
+        not_spatial_ensembles = variables["available_grids"].apply(lambda x: list(set(x) - set(spatial_ensembles)))
+        not_spatials = set(itertools.chain.from_iterable(not_spatial_ensembles.values))
+        individuals_ensembles = { res:variables["available_grids"].apply(lambda x: ensemble in x).sum() for ensemble in not_spatials}
+        return individuals_ensembles
+    return {}
+
+
+def get_df_catalogo():
+    '''
+    crea un DF con el catálogo 'oficial'. Para pruebas, este último es ahora un .csv local
+    pero en un futuro puede ser una BD o un objeto en MINIO.
+    '''
+    catalogo = pd.read_csv("catalogo.csv")
+    catalogo.fillna("", inplace=True) #read_csv llena con NaNs los vacios. con esto, los regresamos a vacios.
+    return catalogo
+
+spatial_ensembles = get_spatial_ensembles_names(url_spatial_ensembles)
+catalogo = get_df_catalogo()
+
 
 app = Flask(__name__)
 
@@ -29,23 +63,14 @@ def hello_world():
     return "<p>Hello!</p>"
 
 
-@app.route("/me")
-def me_api():
-    return {
-        "db": "INEGI",
-        "fuente": "inegi",
-        "url" : "https://chilam.c3.unam.mx/inegi-db"
-    }
-
-
 @app.route("/catalogo")
 def dsources_api():
-    mallas = catalogo["EndPoint"].apply(completa)
-    catalogo["Mallas"] = mallas
-    catalogo["Variables"] = catalogo.apply(lambda x: x["EndPoint"] + "variables" if x["EndPoint"] != "" else "", axis=1)
+    catalogo["spatial_ensembles"] = catalogo["EndPoint"].apply(get_spatial_ensembles_from_source,
+                                                               spatial_ensembles=spatial_ensembles)
+    catalogo["individuals_ensembles"] = catalogo["EndPoint"].apply(get_individuals_ensembles_from_source,
+                                                                   spatial_ensembles=spatial_ensembles) 
     return Response(response=catalogo.to_json(orient="records"), status=200, mimetype="application/json")
 
 
 if __name__ == "__main__":
         app.run_server(debug=True, port=8051, host='0.0.0.0')
-
