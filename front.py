3ea5e1c314918631601452fb4f532de2777dd2bd
Jose Luis Gordillo Ruiz <jlgr@c3.unam.mx>
2025-10-03 15:10:00 -0600

    version beta de front simple para el catálogo

--- front.py
+++ front.py
@@ -4,24 +4,30 @@ import re
 from dash import Dash, html, dash_table
 import json
 import pandas as pd
+import os
 
 
-catalogo_json = requests.get("http://10.90.0.83:8059/catalogo").json()
-#catalogo_DF = pd.json_normalize(catalogo_json, record_prefix="Not")
+
+url_catalogo = os.getenv("URL_CATALOGO_SERVICE")
+url_mallas = os.getenv("URL_MALLAS")
+catalogo_json = requests.get(f'{url_catalogo}/catalogo').json()
 catalogo_DF = pd.json_normalize(catalogo_json)
 catalogo_DF.fillna(0, inplace=True)
-
-print(catalogo_DF.columns)
-columnas_espaciales = ['Name', 'EndPoint']
+catalogo_DF["metainfo"] = catalogo_DF.apply(lambda x: f'[{x["meta.info"]}]({x["meta.url"]})', axis=1)
+catalogo_DF["EndPoint"] = catalogo_DF["EndPoint"].apply(lambda x:  f"[{x}]({x}/variables)" if x != "" else "" )
+columnas_espaciales = ['name', 'description', 'metainfo','EndPoint']
 columnas_espaciales.extend(catalogo_DF.filter(like="spatial_ensembles", axis=1).columns.values)
-columnas_individuales = ['Name',  'EndPoint']
+columnas_individuales = ['name',  'description', 'metainfo','EndPoint']
 columnas_individuales.extend(catalogo_DF.filter(like="individuals_ensembles", axis=1).columns.values)
 
 df_espaciales = catalogo_DF[columnas_espaciales].copy()
 df_espaciales = df_espaciales.loc[(df_espaciales.filter(like="spatial",axis=1)!=0).any(axis=1)]
 df_individuos = catalogo_DF[columnas_individuales].copy()
 df_individuos = df_individuos.loc[(df_individuos.filter(like='individuals_ensembles', axis=1)!=0).any(axis=1)]
-catalogo_DF["EndPoint"] = catalogo_DF["EndPoint"].apply(lambda x:  "[Info de la fuente de datos](" + x + ")" if x != "" else "" )
+
+
+servicios = [{"Nombre":"Catalogo", "url":f'[{url_catalogo}]({url_catalogo}/catalogo)'},
+              {"Nombre":"Ensambles espaciales", "url":f'[{url_mallas}]({url_mallas})'}]
 # Initialize the app
 app = Dash()
 
@@ -30,10 +36,12 @@ app.layout = [
     html.Div(children=html.H1("Catálogo de datos CHILAM")),
     html.Div(children=html.H1("Ensambles espaciales")),
     dash_table.DataTable(data=df_espaciales.to_dict('records'), page_size=10,
-                         columns = [{'id': x, 'name': x.replace("spatial_ensembles.",""), 'presentation': 'markdown'}  for x in df_espaciales.columns]),
+                         columns = [{'id': x, 'name': x.replace("spatial_ensembles.","NumVars en "), 'presentation': 'markdown'}  for x in df_espaciales.columns]),
     html.Div(children=html.H1("Ensambles de individuos")),
     dash_table.DataTable(data=df_individuos.to_dict('records'), page_size=10,
-                         columns = [{'id': x, 'name': x.replace("individuals_ensembles.",""), 'presentation': 'markdown'}  for x in df_individuos.columns])
+                         columns = [{'id': x, 'name': x.replace("individuals_ensembles.","Numvars en "), 'presentation': 'markdown'}  for x in df_individuos.columns]),
+    html.Div(children=html.H1("Servicios")),
+    dash_table.DataTable(data=servicios, columns = [{"id":"Nombre", 'name':"nombre"}, {"id":"url", 'name':"url", "presentation":"markdown"} ])
 ]
 
 # Run the app
